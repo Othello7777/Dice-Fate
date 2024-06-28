4d37b4bdfb2dbf3e9202f5d27c8098e995c7ab5a
Othello <67707230+Othello7777@users.noreply.github.com>
2024-06-28 13:36:52 +0200

    Creating new project.
    Update files.

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./App.css";
+import {
+	Attribute,
+	CharacterTree,
+	DescriptionPlayer,
+	Header,
+	HealthBar,
+	Reset,
+	valueBarsState,
+	attrsState,
+	treesState,
+} from "./compoments";
+
+const lpMax = 56;
+
+const App = () => {
+	const [attrs, setAttrs] = useState(attrsState);
+	const [lpPlayers, setLpPlayers] = useState(lpMax);
+
+	const reset = (attrs, setLpPlayers, lpMax) => {
+		attrs.forEach((attr) => (attr.value = 3));
+		setLpPlayers(lpMax);
+	};
+
+	const addDice = (attrs) => {
+		attrs.forEach((attr) => (attr.value = 3));
+	};
+
+	return (
+		<div className='App'>
+			<Header />
+			<br></br>
+			<div className='redBox'>
+				<div className='visualBox'>
+					<div className='puzzleFlex'>
+						<div className='puzzleBox'>
+							<div style={{ color: "black" }} className='shadowText'>
+								Punkty nauki: {lpPlayers}
+							</div>
+						</div>
+						<Reset
+							text={"Reset"}
+							handleClick={() => reset(attrs, setLpPlayers, lpMax)}
+						/>
+						<Reset text={"dodaj kość"} handleClick={() => addDice(attrs)} />
+					</div>
+					{attrs.map((value, index) => (
+						<Attribute
+							index={index}
+							attrs={attrs}
+							setAttrs={setAttrs}
+							lpPlayers={lpPlayers}
+							setLpPlayers={setLpPlayers}
+						/>
+					))}
+				</div>
+			</div>
+			<br></br>
+			{/* <div className='blueBox'>
+				{trees.map((value, index) => (
+					<CharacterTree index={index} trees={trees} setTrees={setTrees} />
+				))}
+			</div> */}
+		</div>
+	);
+};
+
+export default App;
